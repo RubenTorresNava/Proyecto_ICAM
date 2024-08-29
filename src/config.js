e7358e536248fa6104ab4ae4de264b7832160187
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-08-28 21:40:14 -0600

    chore: Update MONGO_URI in config.js to use local MongoDB server

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ config();
 
 export const JWT_SECRET = process.env.JWT_SECRET || "miclavesecreta";
 export const PORT = process.env.PORT || 3000;
-export const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://icam:icam246@icam.gvbq5.mongodb.net/?retryWrites=true&w=majority&appName=ICAM";
+export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/icam";
