3ed8f7f14c0f87b45c3749300e09597d3bcb5c96
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-08-29 18:18:04 -0600

    chore: Update config.js to remove default values for JWT_SECRET, PORT, and MONGO_URI

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import { config } from "dotenv";
 
 config();
 
-export const JWT_SECRET = process.env.JWT_SECRET || "miclavesecreta";
-export const PORT = process.env.PORT || 3000;
-export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/icam";
+export const JWT_SECRET = process.env.JWT_SECRET
+export const PORT = process.env.PORT
+export const MONGO_URI = process.env.MONGO_URI
